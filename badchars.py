ddb77220d8aa1433881ee7ac986ef20c24f7c283
Peter <github@peter.dev.br>
2024-03-26 04:36:08 -0300

    feat: badchars generators module

--- badchars.py
+++ badchars.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
 
-badchars = []
 
-for num in range(1, 256):
-    print(f"\\x{hex(num)}", end="")
+def generate_badchars(low = 1, high = 256):
+    badchars = []
+
+    for num in range(low, high):
+        badchars.append(hex(num).replace("0x", "\\x"))
+        
+    return badchars
+
+
+if __name__ == "__main__":
+    for char in generate_badchars():
+        print(char, end="")
